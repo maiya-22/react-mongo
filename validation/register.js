4dce353b0deb168be3becfce4360ab6b373e0938
maiya-22 <maiya_o@yahoo.com>
2018-06-11 16:51:03 -0400

    sign up working, validate name-length not working

--- validation/register.js
+++ validation/register.js
@@ -3,6 +3,7 @@ const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+  //   check Validator:
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be over 2 chars and under 30 chars";
   }
