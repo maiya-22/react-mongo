867330cfce6c7aaa5939753eb4d6b63d105bc2ab
maiya-22 <maiya_o@yahoo.com>
2018-06-11 09:17:50 -0400

    MongoDB connected

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB config
+
+const db = require("./config/keys").mongoURI;
+
+// connect to Mongo db:
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("hello");
