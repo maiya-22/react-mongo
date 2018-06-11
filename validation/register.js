976f7457afc637c9b4510c0b063397b214cca1d9
maiya-22 <maiya_o@yahoo.com>
2018-06-11 16:21:35 -0400

    saving progress

---
+++ validation/register.js
@@ -0,0 +1,13 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be over 2 chars and under 30 chars";
+  }
+  return {
+    errors,
+    isValid: isEmpty
+  };
+};
