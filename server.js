a7ec4d7eb28358edb6cdabcc6f439e48ced78466
maiya-22 <maiya_o@yahoo.com>
2018-06-11 22:42:27 -0400

    profile model in place

--- server.js
+++ server.js
@@ -8,29 +8,31 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-// body-parser middleware:
+
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// DB config
+
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-// connect to Mongo db:
+// Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-app.use(passport.initialize());
-require("./config/passport")(passport);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  `server running on port ${PORT}`;
-});
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
